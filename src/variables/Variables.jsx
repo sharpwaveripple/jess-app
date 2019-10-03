50749d442c2ae134b958ecfa459637f2dd2a0017
sharpwaveripple <jonathantay193@gmail.com>
2019-10-03 15:56:09 +0100

    customised tables

--- src/variables/Variables.jsx
+++ src/variables/Variables.jsx
@@ -266,14 +266,12 @@ var style = {
 // // // For tables
 // //
 //
-const thArray = ["ID", "Name", "Salary", "Country", "City"];
+
+const thArray = ["ID", "Name", "Points", "City"];
 const tdArray = [
-  ["1", "Dakota Rice", "$36,738", "Niger", "Oud-Turnhout"],
-  ["2", "Minerva Hooper", "$23,789", "Curaçao", "Sinaai-Waas"],
-  ["3", "Sage Rodriguez", "$56,142", "Netherlands", "Baileux"],
-  ["4", "Philip Chaney", "$38,735", "Korea, South", "Overland Park"],
-  ["5", "Doris Greene", "$63,542", "Malawi", "Feldkirchen in Kärnten"],
-  ["6", "Mason Porter", "$78,615", "Chile", "Gloucester"]
+  ["1", "Hayley", "$36,738", "Edinburgh"],
+  ["2", "Anna", "$23,789", "Somewhere in Australia"],
+  ["3", "Marco", "$56,142", "Planet Marco"],
 ];
 
 //
